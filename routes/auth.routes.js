a817a7cabf1cd744c01874594d78d113040116e0
David Bassey <dondave.bassey23@gmail.com>
2025-11-25 17:44:16 +0000

    made changes in auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { Signup, Signin } from '../controllers/auth.controller.js';
+import { Signup, Signin } from '../controllers/user.controllers.js';
 
 
 const router = express.Router();
